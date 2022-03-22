fe7a5bcf3ec1ba42ea0ba21e0b2b959484d89a1f
z0f0rt <greenboots@bk.ru>
2022-03-22 03:45:26 +0300

    New tasks on cicles and arrays and functions

---
+++ 15.03.22/arr1.js
@@ -0,0 +1,18 @@
+"use strict";
+let arr = [3, 4, 6, -10, 335, 69, -228, 18];
+let x = 1;
+let y;
+let newArr = [];
+for (let i = 0; i < arr.length; i++) {
+  y = arr[i] + x;
+  newArr.push(y);
+}
+console.log(newArr);
+
+// const n = 10;
+// if (n == 10) {
+//   console.log("Ты готов!");
+// }
+// for (let i = 0; i <= n; i++) {
+//   console.log(i);
+// }
